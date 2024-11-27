6501949281efcb9faf014eab9f7b5d169f89a5ff
Edgar Cano M. <edgarrubendario@gmail.com>
2024-11-27 08:38:48 -0300

    crear usuario y rol en el seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /**crear usuario y asignar rol */
+        $u = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $role = Role::create(['name' => 'Super Admin']);
+        $u->assignRole($role);
     }
 }
